2f6358c0202bffa28dbb3c597ae818021b789832
kjetilge <geirbo@icloud.com>
2022-08-16 16:57:23 +0200

    added hosting

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "nextprofileappd4b02725": {
+        "nextprofileapp397c8866": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
             "UserPoolId": "string",
